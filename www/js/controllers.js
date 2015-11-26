6ab37b96c0ebb47c9e9b17655a8bb85eee5f6977
morkalork <maffelu@gmail.com>
2015-11-26 16:09:12 +0100

    Fixed the item list structure and appearance.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,6 @@
 angular.module('starter.controllers', ['ngCordova', 'starter.repositories', 'ngLodash'])
-	.controller('ToDoListCtrl', ['$scope', '$rootScope', '$ionicModal', '$cordovaSQLite', 'taskRepository', 'lodash', function ($scope, $rootScope, $ionicModal, $cordovaSQLite, taskRepository, lodash) {
+	.controller('ToDoListCtrl', ['$scope', '$rootScope', '$ionicModal', '$cordovaSQLite', 'taskRepository', 'lodash', 
+	function ($scope, $rootScope, $ionicModal, $cordovaSQLite, taskRepository, lodash) {
 
 		$scope.items = [];
 
@@ -17,6 +18,30 @@ angular.module('starter.controllers', ['ngCordova', 'starter.repositories', 'ngL
 			console.error("SQL ERROR: %o", error);
 		}
 
+		var calculateNewStatus = function (status) {
+			switch (status) {
+				case 0: return 1;
+				case 1: return 0;
+				default: return 0;
+			}
+		}
+
+		var getIcon = function (icon) {
+			var platform = "android";
+			var isIos = ionic.Platform.isIOS();
+			if (isIos) {
+				platform = "ios";
+			}
+			var iconName = "";
+
+			switch (icon) {
+				case "checkbox": iconName = isIos ? "circle-outline" : "checkbox-outline-blank"; break;
+				case "checkbox-checked": iconName = isIos ? "circle-filled" : "checkbox-outline"; break;
+			}
+
+			return "ion-" + platform + "-" + iconName;
+		}
+
 		$scope.openModal = function () {
 			$scope.modal.show();
 		};
@@ -29,10 +54,32 @@ angular.module('starter.controllers', ['ngCordova', 'starter.repositories', 'ngL
 			$scope.modal.remove();
 		});
 
+		$scope.getButtonStatusClass = function (status) {
+			switch (status) {
+				case 0:
+					var buttonIcon = getIcon("checkbox");
+					return buttonIcon;
+				case 1:
+					var buttonIcon = getIcon("checkbox-checked");
+					return buttonIcon;
+				default:
+					return "broken";
+			}
+		};
+
+		$scope.getItemStatusClass = function (status) {
+			switch (status) {
+				case 0:
+					return "unfinished-task";
+				case 1:
+					return "finished-task";
+				default:
+					return "broken";
+			}
+		};
+
 		$scope.AddItem = function (data) {
 			var itemArray = [data.itemTitle, 0];
-
-			console.log("Add item: %o", itemArray);
 
 			taskRepository
 				.addTask(itemArray)
@@ -48,14 +95,29 @@ angular.module('starter.controllers', ['ngCordova', 'starter.repositories', 'ngL
 
 			$scope.closeModal();
 			createModal(); //Because once closed, a modal can never be opened again (ffs...)
-		}
+		};
 
 		$scope.setTodoStatus = function (id, status) {
-		}
+
+			status = calculateNewStatus(status);
+
+			taskRepository
+				.updateTaskStatus(id, status)
+				.then(function (result) {
+
+
+					lodash.each($scope.items, function (data, idx) {
+						if (data.id === id) {
+							data.status = status;
+							return;
+						}
+					});
+
+					$scope.$apply();
+				}, sqlErrorHandler);
+		};
 
 		$scope.removeTodoItem = function (id) {
-			console.log("Remove item: %i", id);
-			console.log($scope.items);
 			taskRepository
 				.removeTask(id)
 				.then(function () {
@@ -72,9 +134,8 @@ angular.module('starter.controllers', ['ngCordova', 'starter.repositories', 'ngL
 						$scope.$apply();
 					}
 
-					console.log("Items: %o", $scope.items);
 				}, sqlErrorHandler);;
-		}
+		};
 		
 		//Initial push
 		$rootScope.$on('databaseLoaded', function () {
